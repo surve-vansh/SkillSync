4ea21c7f45b737ba1f73e377312e3ddac421801d
Surve Vansh <vanshsurve30@gmail.com>
2026-09-18 13:58:57 +0530

    Allow local and production CORS

--- server/index.js
+++ server/index.js
@@ -33,7 +33,10 @@ app.use(cookieParser());
 
 app.use(cors({
     // origin: "http://localhost:5173",
-    origin: "https://skillsync-lime.vercel.app",
+    origin: [
+      "https://skillsync-lime.vercel.app",
+      "http://localhost:5173"
+    ],
     credentials: true,
 }));
 
@@ -79,7 +82,7 @@ const { Server } = require('socket.io');
 const server = http.createServer(app);
 const io = new Server(server, {
   // cors: { origin: 'http://localhost:5173', credentials: true }
-  cors: { origin: 'https://skillsync-lime.vercel.app', credentials: true }
+  cors: { origin: ['https://skillsync-lime.vercel.app','http://localhost:5173'], credentials: true }
 });
 
 io.on('connection', (socket) => {
